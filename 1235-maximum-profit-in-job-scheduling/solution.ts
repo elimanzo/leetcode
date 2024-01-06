01ec9cbac55816e01198d90928842d103f027cc6
Eli Manzo <96manzo.eli@gmail.com>
2024-01-05 21:11:46 -0800

    Typescript solution for 1235-maximum-profit-in-job-scheduling

---
+++ 1235-maximum-profit-in-job-scheduling/solution.ts
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/maximum-profit-in-job-scheduling/
+// 1235-maximum-profit-in-job-scheduling
+
+function jobScheduling(startTime: number[], endTime: number[], profit: number[]): number {
+  const n = startTime.length;
+  const intervals = Array(n).fill(null).map((_, i) => [startTime[i], endTime[i], profit[i]]);
+  intervals.sort((a, b) => a[0] - b[0]);
+  const cache = {};
+  const binarySearch = (target: number): number => {
+    let lo = 0;
+    let hi = n;
+    while (lo + 1 !== hi) {
+      const mid = Math.floor((lo + hi) / 2);
+      if (intervals[mid][0] < target) {
+        lo = mid;
+      } else {
+        hi = mid;
+      }
+    }
+    return hi;
+  };
+
+  const solve = (i: number): number => {
+    if (i === n) {
+      return 0;
+    }
+    if (cache[i] != null) {
+      return cache[i];
+    }
+
+    // don't include
+    let res = solve(i + 1);
+
+    // include
+    const nextJobIndex = binarySearch(intervals[i][1]);
+    res = Math.max(res, intervals[i][2] + solve(nextJobIndex));
+
+    return cache[i] = res;
+  };
+  return solve(0);
+};
