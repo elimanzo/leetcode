0fe2dcbd17e99b176befa141997a61f2eaa5e91e
Eli Manzo <96manzo.eli@gmail.com>
2024-01-28 11:03:17 -0800

    Update TS solution for 0629-k-inverse-pairs-array

--- 0629-k-inverse-pairs-array/solution.ts
+++ 0629-k-inverse-pairs-array/solution.ts
@@ -1,23 +1,24 @@
 // https://leetcode.com/problems/k-inverse-pairs-array/
-// 0629-k-inverse-pairs-arra
+// 0629-k-inverse-pairs-array
 
-const MOD = 10**9 + 7;
+const MOD = 10 ** 9 + 7;
 
 function kInversePairs(n: number, k: number): number {
-  let prev = Array(k + 1).fill(0);
-  prev[0] = 1;
+  let prev = new Array(k + 1).fill(0);
+  prev[0]= 1;
 
-  for(let i = 1; i < n + 1; i++) {
+  for (let N = 1; N < n + 1; N++) {
     const cur = Array(k + 1).fill(0);
     let total = 0;
-    for(let j = 0; j < k + 1; j++) {
-      if(j >= i) {
-        total -= prev[i - j]
+    for (let K = 0; K < k + 1; K++) {
+      if(K >= N) {
+        total -= prev[K - N] - MOD;
       }
-      total = (total + prev[j]) % MOD;
-      cur[j] = total;
+      total = (total + prev[K] + MOD) % MOD;
+      cur[K] = total;
     }
     prev = cur;
   }
+
   return prev[k];
 };
