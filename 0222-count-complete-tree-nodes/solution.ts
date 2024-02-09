f7c65765e9847e7dc92cc63c534f3dc2c114c001
Eli Manzo <96manzo.eli@gmail.com>
2024-02-09 14:52:12 -0800

    TS solution for 0222-count-complete-tree-nodes

---
+++ 0222-count-complete-tree-nodes/solution.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/count-complete-tree-nodes/
+// 0222-count-complete-tree-nodes
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function countNodes(root: TreeNode | null): number {
+  return !root ? 0 : 1 + countNodes(root.left) + countNodes(root.right);
+};
